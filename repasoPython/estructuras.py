5eb83ebb8936343996360f4f70a477bcfd11441a
AstridMartinez <martinezastrid730@gmail.com>
2023-03-08 18:35:06 -0500

    Se crea el archivo estructuras

---
+++ repasoPython/estructuras.py
@@ -0,0 +1,55 @@
+#Repetitiva
+"""
+Escribir un programa que solicite ingresar la nota de 10 alumnos, el programa debe
+informar de cuantos han aprobado y cuantos han suspendido.
+
+"""
+"""
+x=0
+total=0
+while x<10:
+    nota=int(input("Ingrese la nota: "))
+    if nota>4:
+        total=total+1
+    x=x+1
+print("Han aprobado ",total)
+"""
+#for
+"""
+Desarrollar un programa que solicite la carga de 10 
+números e imprima la suma de los últimos 5 valores ingresados.
+suma=0
+"""
+"""
+for f in range(10):
+    valor = int(input("Ingrese un valor: "))
+    if f>4:
+        suma = suma+valor
+print("La suma de los últimos 5 valores es: ")
+print(suma)
+"""
+#selectivas
+#simple
+
+edad = 21 # asignamos un valor a la variable edad
+
+if (edad > 18):
+
+  print("Es mayor de edad")
+
+#dobles
+edad = 15 # asignamos un valor a la variable edad
+
+if (edad > 17):
+
+  print("Es mayor de edad")
+
+else:
+
+  print("Es menor de edad")
+
+
+
+
+
+
