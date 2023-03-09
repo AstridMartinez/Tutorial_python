0eb74b15669cc18847f9169ce5f974b6ebeafe5a
AstridMartinez <martinezastrid730@gmail.com>
2023-03-09 14:18:46 -0500

    Se actualiza el archivo conversiones

---
+++ repasoPython/conversiones.py
@@ -0,0 +1,23 @@
+#Conversión de datos
+#Conversión de cadenas a números enteros o flotantes----------------
+num_entero = int("10")
+num_flotante = float("3.1416")
+print(num_entero)
+print(num_flotante)
+
+#Conversión de números a cadenas------------------------------------
+num = 123
+cadena = str(num)
+print(cadena)
+
+#De lista a cadena----------------------------------------------------
+lista = ['H', 'o', 'l', 'a']
+cadena = "".join(lista)
+print(cadena)
+
+#Conversión de enteros a binarios o hexadecimales---------------------
+num_entero = 40
+num_binario = bin(num_entero)
+num_hexadecimal = hex(num_entero)
+print(num_binario)
+print(num_hexadecimal)
