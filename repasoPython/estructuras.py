cd3218059b8019ade317248529b4f899e5c22af0
AstridMartinez <martinezastrid730@gmail.com>
2023-03-09 13:17:00 -0500

    Se modifica el archivo estructuras

--- repasoPython/estructuras.py
+++ repasoPython/estructuras.py
@@ -1,41 +1,11 @@
-#Repetitiva
-"""
-Escribir un programa que solicite ingresar la nota de 10 alumnos, el programa debe
-informar de cuantos han aprobado y cuantos han suspendido.
-
-"""
-"""
-x=0
-total=0
-while x<10:
-    nota=int(input("Ingrese la nota: "))
-    if nota>4:
-        total=total+1
-    x=x+1
-print("Han aprobado ",total)
-"""
-#for
-"""
-Desarrollar un programa que solicite la carga de 10 
-números e imprima la suma de los últimos 5 valores ingresados.
-suma=0
-"""
-"""
-for f in range(10):
-    valor = int(input("Ingrese un valor: "))
-    if f>4:
-        suma = suma+valor
-print("La suma de los últimos 5 valores es: ")
-print(suma)
-"""
-#selectivas
+#selectivas----------------------------------------------------
 #simple
 
 edad = 21 # asignamos un valor a la variable edad
 
 if (edad > 18):
 
-  print("Es mayor de edad")
+ print("Es mayor de edad")
 
 #dobles
 edad = 15 # asignamos un valor a la variable edad
@@ -48,6 +18,35 @@ else:
 
   print("Es menor de edad")
 
+  #multiple
+edad = 25
+if edad < 17:
+    print("Es menor de edad")
+elif edad >17:
+    print("Es mayor de edad")
+else:
+    print("edad")
+
+#Repetitiva-------------------------------------------------
+
+#while
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+#for
+frutas = ["manzana", "pera", "banana"]
+for fruta in frutas:
+    print(fruta)
+
+#while con condicion de salida
+i = 0
+while True:
+    print(i)
+    i += 1
+    if i == 5:
+        break
 
 
 
